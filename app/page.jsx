968798f4560dccbd7bc09cd9c29cf89d5e42d25b
Yunier Yau <yauyunier@gmail.com>
2024-04-27 11:58:09 -0500

    Se agrego la carpeta componentes

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,9 @@
+import Micomponente from "@/components/Micomponente";
+
 export default function Home() {
   return (
     <main>
-      <h1 className="text-5xl font-bold text-ctext">
-        Panama<span className="text-primary">BeerClub</span>
-      </h1>
+      <Micomponente />
     </main>
   );
 }
---
+++ components/Micomponente.jsx
@@ -0,0 +1,7 @@
+export default function Micomponente() {
+  return (
+    <h1 className="text-5xl font-bold text-ctext">
+      Panama<span className="text-primary">BeerClub</span>
+    </h1>
+  );
+}
